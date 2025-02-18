593a8aff6d87fb489125929bdc6460cf562f7d02
Gianni Russo <djanibek.k@gmail.com>
2025-02-18 13:11:24 +0300

    Change in api.py

--- api.py
+++ api.py
@@ -3,19 +3,30 @@ from fastapi import FastAPI
 import numpy as np
 import pickle
 import scipy.sparse
+import os
+
+if not os.path.exists("model.pkl") or not os.path.exists("interaction_matrix.npz"):
+    raise FileNotFoundError("Файлы модели отсутствуют! Проверь загрузку model.pkl и interaction_matrix.npz")
 
 # Инициализируем FastAPI
 app = FastAPI()
 
 # Загружаем обученную модель ALS (должен быть файл `model.pkl` в проекте)
 try:
+    print("Загрузка модели ALS...")
     with open("model.pkl", "rb") as f:
         model = pickle.load(f)
     interaction_matrix = scipy.sparse.load_npz("interaction_matrix.npz")
+    
+    if model is None or interaction_matrix is None:
+        raise ValueError("Ошибка: model или interaction_matrix загрузились как None!")
+        
+    print("Модель успешно загружена!")
 except Exception as e:
     print(f"Ошибка загрузки модели: {e}")
     model = None
     interaction_matrix = None
+
 
 # Эндпоинт для получения рекомендаций
 @app.get("/recommend/{visitorid}")
